5ccfc9fddca2e28605afca08770ea01440fd167c
engineersworkbook <shivamkalra2013@gmail.com>
2017-12-20 19:47:30 +0530

    Added Show camera class

--- app/src/main/java/com/example/shivamkalra/shivam/MainActivity.java
+++ app/src/main/java/com/example/shivamkalra/shivam/MainActivity.java
@@ -1,15 +1,22 @@
 package com.example.shivamkalra.shivam;
 
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.hardware.Camera;
+import android.media.VolumeShaper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.SurfaceView;
 import android.widget.EditText;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     SurfaceView camera;
     EditText result;
+    Camera cam;
+    FrameLayout frame;
+    ShowCamera showCamera;
 
 
     @Override
@@ -19,7 +26,31 @@ public class MainActivity extends AppCompatActivity {
 
     camera = (SurfaceView) findViewById(R.id.SurfaceView);
     result = (EditText) findViewById(R.id.Edit);
+    frame =(FrameLayout) findViewById(R.id.frame);
+        showCamera = new ShowCamera(getApplicationContext(),camera);
+     frame.addView();
 
-        Intent camera
+        cam = switch_on_camera();
+    }
+
+
+    //For returning camera object to the cam View
+    public Camera switch_on_camera(){
+        Camera cam_obj = null;
+
+        //For opening Camera
+        cam_obj = Camera.open();
+        Camera.Parameters parameters =cam_obj.getParameters();
+
+        if(this.getResources().getConfiguration().orientation!= Configuration.ORIENTATION_LANDSCAPE){
+            parameters.set("orientation","portrait");
+            cam_obj.setDisplayOrientation(90);
+            parameters.setRotation(90);
+        }else{
+            parameters.set("orientation","landscape");
+            cam_obj.setDisplayOrientation(0);
+            parameters.setRotation(0);
+        }
+     return cam_obj;
     }
 }
---
+++ app/src/main/java/com/example/shivamkalra/shivam/ShowCamera.java
@@ -0,0 +1,8 @@
+package com.example.shivamkalra.shivam;
+
+/**
+ * Created by hp on 20-12-2017.
+ */
+
+public class ShowCamera {
+}
